b1e11ee824167f48c35f5af9a72be9eb40add649
othierrydaora <thierryalefhsanto@gmail.com>
2022-05-19 20:57:49 -0300

    Tela de cadastro arrumada

--- site/src/pages/cadastro/index.js
+++ site/src/pages/cadastro/index.js
@@ -3,37 +3,35 @@ import Header from '../../common/components/Header';
 
 export default function Cadastro() {
     return (
-        <main className='main'>
+        <div className='cadastro'>
             <Header back="show" />
 
-            <div className='main-cont'>
-                    <div>
-                        <div className='title'>CADASTRO</div>
-                        <div className='forms-cont'>
-                            <div>
-                                <input type='text' placeholder='Nome'></input>
-                                <input type='text' placeholder='Nascimento'></input>
-                            </div>
-                        <div>
-                            <input type='text' placeholder='CPF'></input>
-                            <input type='text' placeholder='Email'></input>
-                        </div>
-                        <div>
-                            <input type='text' placeholder='Endereço'></input>
-                                <input type='text' placeholder='Telefone'></input>
-                        </div>
-                        <div>
-                            <input type='text' placeholder='Veículo'></input>
-                            <input type='text' placeholder='Preço'></input>
-                        </div>
-                        <div>
-                            <input type='text' placeholder='Placa'></input>
-                            <input type='text' placeholder='Data do registro'></input>
-                        </div>
-                        <button className='sending-btn'>ENVIAR</button>
-                    </div>
+            <div className='forms-cont'>
+                <div className='title'>Cadastro de Vendas</div>
+                
+                <div>
+                    <input type='text' placeholder='Nome'></input>
+                    <input type='text' placeholder='Nascimento'></input>
                 </div>
+                <div>
+                    <input type='text' placeholder='CPF'></input>
+                    <input type='text' placeholder='Email'></input>
+                </div>
+                <div>
+                    <input type='text' placeholder='Endereço'></input>
+                        <input type='text' placeholder='Telefone'></input>
+                </div>
+                <div>
+                    <input type='text' placeholder='Veículo'></input>
+                    <input type='text' placeholder='Preço'></input>
+                </div>
+                <div>
+                    <input type='text' placeholder='Placa'></input>
+                    <input type='text' placeholder='Data do registro'></input>
+                </div>
+                
+                <button className='sending-btn'>ENVIAR</button>
             </div>
-        </main>
+        </div>
     );
 }
