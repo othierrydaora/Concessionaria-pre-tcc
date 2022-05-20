af9c7849dfc80357435552d4545b0dac050e8b9b
Thierry <91567505+othierrydaora@users.noreply.github.com>
2022-05-20 11:03:23 -0300

    Update sistemaSigma.sql

--- Scripts SQL/sistemaSigma.sql
+++ Scripts SQL/sistemaSigma.sql
@@ -11,27 +11,24 @@ CREATE TABLE TB_Cliente (
         DS_Telefone     VARCHAR(20) 
 );
 
-
-
 CREATE TABLE TB_Venda (
-
         ID_Venda      INT PRIMARY KEY auto_increment,
         NM_MODELO     VarChar(100),
         NR_PLACA      VarChar(100),
         DS_EMAIL      VarChar(100),
         VL_PRECO      DECIMAL(10, 2),
         DT_COMPRA     DATETIME,
-        ID_CLIENTE    VARCHAR(100)
-
+        ID_Cliente	  INT,
+        FOREIGN KEY (ID_Cliente) REFERENCES TB_Cliente(ID_Cliente)
 );
 
 CREATE TABLE TB_Funcionario (
         
-        ID_FUNCIONARIO    INT PRIMARY KEY,
-        NM_NOME           VarChar(100),
-        DS_EMAIL          VarChar(100),
-        NR_SENHA          VarChar(20),
-        ID_CPF            
-        ID_VALOR
+        ID_FUNCIONARIO  INT PRIMARY KEY,
+        NM_NOME         VarChar(100),
+        DS_EMAIL        VarChar(100),
+        NR_SENHA        VarChar(20),
+        DS_CPF          VARCHAR(11),
+        FOREIGN KEY (ID_Venda) REFERENCES TB_Venda (ID_Venda)
+);
 
-)
