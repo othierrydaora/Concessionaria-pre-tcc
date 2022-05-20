1c08c57843cb27a2b16133cf0ebe6c2892bfb732
othierrydaora <thierryalefhsanto@gmail.com>
2022-05-19 21:05:27 -0300

    Ajustes finos no menu

--- site/src/pages/menu/index.js
+++ site/src/pages/menu/index.js
@@ -11,14 +11,14 @@ export default function Menu() {
                 <div className='card'>
                     <img className='img-card' src='./images/Login-rafiki.svg' alt=''/>
                     <h1 className='card-txt'>Cadastro</h1>
-                    <p className='card-paragrafo'>Realize o cadastro de um novo cliente</p>
+                    <p className='card-paragrafo'>Realize o cadastro de uma nova venda</p>
                     <Link to='/cadastro'><div className='btn-card'>Cadastrar</div></Link>
                 </div>
 
                 <div className='card'>
                     <img className='img-card' src='./images/Version control-bro.svg' alt=''/>
                     <h1 className='card-txt'>Gerenciamento</h1>
-                    <p className="card-paragrafo">Faça a consulta e o gerenciamento de dados de clientes</p>
+                    <p className="card-paragrafo">Faça a consulta e o gerenciamento de dados de vendas passadas</p>
                     <Link to='/gerenciamento'><div className='btn-card'>Gerenciar</div></Link>
                 </div>
         </section>
