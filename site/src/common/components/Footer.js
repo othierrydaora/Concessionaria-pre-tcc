b514798e1d7b88ddb550bdb0bfcbc6bba618e7a1
othierrydaora <thierryalefhsanto@gmail.com>
2022-05-19 21:12:47 -0300

    Linkagem dos perfis do git

--- site/src/common/components/Footer.js
+++ site/src/common/components/Footer.js
@@ -16,14 +16,14 @@ export default function Footer() {
                     </ul>
                     <ul class="dev-names">
                         <a href="https://github.com/DOLK2K"><li>Vinicius</li></a>
-                        <li>Rodrigo</li>
+                        <a href="https://github.com/RodrigoDig"><li>Rodrigo</li></a>
                     </ul>
                 </div>
                 <div class="footer-column">
                     <div className="footer-column-name">Contato</div>
                     <ul>
                         <a href="mailto:sigmacarros@outlook.com"><li>sigmacarros@outlook.com</li></a>
-                        <li>Tel: (11) 9999-99999</li>
+                        <li>Tel: (11) 99999-9999</li>
                         <li>Av. Constelar, 401</li>
                     </ul>
                 </div>
