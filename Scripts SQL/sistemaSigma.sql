002db7ec9558bc0fc1db792a416c48b249f6eea2
Thiago Rodrigues <contatopixthiago@gmail.com>
2022-05-20 05:49:38 -0300

    Update sistemaSigma.sql

--- Scripts SQL/sistemaSigma.sql
+++ Scripts SQL/sistemaSigma.sql
@@ -10,3 +10,28 @@ CREATE TABLE TB_Cliente (
         DS_Endereco     VARCHAR(100),
         DS_Telefone     VARCHAR(20) 
 );
+
+
+
+CREATE TABLE TB_Venda (
+
+        ID_Venda      INT PRIMARY KEY auto_increment,
+        NM_MODELO     VarChar(100),
+        NR_PLACA      VarChar(100),
+        DS_EMAIL      VarChar(100),
+        VL_PRECO      DECIMAL(10, 2),
+        DT_COMPRA     DATETIME,
+        ID_CLIENTE    VARCHAR(100)
+
+);
+
+CREATE TABLE TB_Fyncionario (
+        
+        ID_FUNCIONARIO    INT PRIMARY KEY,
+        NM_NOME           VarChar(100),
+        DS_EMAIL          VarChar(100),
+        NR_SENHA          VarChar(20),
+        ID_CPF            
+        ID_VALOR
+
+)
