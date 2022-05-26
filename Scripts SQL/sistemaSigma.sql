d22c8b7489ca37df3569f73b8ce2d8a2ff93aecc
othierrydaora <thierryalefhsanto@gmail.com>
2022-05-26 15:08:51 -0300

    Falta arrumar as chaves estrangeiras

--- Scripts SQL/sistemaSigma.sql
+++ Scripts SQL/sistemaSigma.sql
@@ -3,32 +3,29 @@ USE sistemaSigmaDB;
 
 CREATE TABLE TB_Cliente (
         ID_Cliente      INT PRIMARY KEY auto_increment,
-        NM_Cliente      VARCHAR(100),
-        DS_Cpf          VARCHAR(14),
-        DT_Nascimento   DATE,
-        DS_Email        VARCHAR(100),
-        DS_Endereco     VARCHAR(100),
-        DS_Telefone     VARCHAR(20) 
+        NM_Cliente      VARCHAR(100) NOT NULL,
+        DS_Cpf          VARCHAR(14) NOT NULL,
+        DT_Nascimento   DATE NOT NULL,
+        DS_Email        VARCHAR(100) NOT NULL,
+        DS_Endereco     VARCHAR(100) NOT NULL,
+        DS_Telefone     VARCHAR(14) NOT NULL
 );
 
 CREATE TABLE TB_Venda (
         ID_Venda      INT PRIMARY KEY auto_increment,
-        NM_MODELO     VarChar(100),
-        NR_PLACA      VarChar(100),
-        DS_EMAIL      VarChar(100),
-        VL_PRECO      DECIMAL(10, 2),
-        DT_COMPRA     DATETIME,
-        ID_Cliente	  INT,
-        FOREIGN KEY (ID_Cliente) REFERENCES TB_Cliente(ID_Cliente)
+        NM_MODELO     VarChar(100) NOT NULL,
+        NR_PLACA      VarChar(100) NOT NULL,
+        DS_EMAIL      VarChar(100) NOT NULL,
+        VL_PRECO      DECIMAL(10, 2) NOT NULL,
+        DT_COMPRA     DATETIME NOT NULL,
+        CONSTRAINT FK_ClienteVenda      FOREIGN KEY (ID_Cliente)        REFERENCES TB_Cliente(ID_Cliente),
+        CONSTRAINT FK_FuncionarioVenda  FOREIGN KEY (ID_Funcionario)    REFERENCES TB_Funcionario(ID_Funcionario)
 );
 
 CREATE TABLE TB_Funcionario (
-        
         ID_FUNCIONARIO  INT PRIMARY KEY,
-        NM_NOME         VarChar(100),
-        DS_EMAIL        VarChar(100),
-        NR_SENHA        VarChar(20),
-        DS_CPF          VARCHAR(14),
-        FOREIGN KEY (ID_Venda) REFERENCES TB_Venda (ID_Venda)
+        NM_NOME         VarChar(100) NOT NULL,
+        DS_EMAIL        VarChar(100) NOT NULL,
+        NR_SENHA        VarChar(20) NOT NULL,
+        DS_CPF          VARCHAR(14) NOT NULL
 );
-
