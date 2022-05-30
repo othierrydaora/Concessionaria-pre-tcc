efd6146169e4a1a8ce68fb4074e4d21dc3194026
othierrydaora <thierryalefhsanto@gmail.com>
2022-05-30 16:37:09 -0300

    script sql arrumado

--- Scripts SQL/sistemaSigma.sql
+++ Scripts SQL/sistemaSigma.sql
@@ -11,21 +11,23 @@ CREATE TABLE TB_Cliente (
         DS_Telefone     VARCHAR(14) NOT NULL
 );
 
-CREATE TABLE TB_Venda (
-        ID_Venda      INT PRIMARY KEY auto_increment,
-        NM_MODELO     VarChar(100) NOT NULL,
-        NR_PLACA      VarChar(100) NOT NULL,
-        DS_EMAIL      VarChar(100) NOT NULL,
-        VL_PRECO      DECIMAL(10, 2) NOT NULL,
-        DT_COMPRA     DATETIME NOT NULL,
-        CONSTRAINT FK_ClienteVenda      FOREIGN KEY (ID_Cliente)        REFERENCES TB_Cliente(ID_Cliente),
-        CONSTRAINT FK_FuncionarioVenda  FOREIGN KEY (ID_Funcionario)    REFERENCES TB_Funcionario(ID_Funcionario)
+CREATE TABLE TB_Funcionario (
+        ID_Funcionario  INT PRIMARY KEY,
+        NM_Funcionario  VarChar(100) NOT NULL,
+        DS_Email        VarChar(100) NOT NULL,
+        NR_Senha        VarChar(20) NOT NULL,
+        DS_Cpf          VARCHAR(14) NOT NULL
 );
 
-CREATE TABLE TB_Funcionario (
-        ID_FUNCIONARIO  INT PRIMARY KEY,
-        NM_NOME         VarChar(100) NOT NULL,
-        DS_EMAIL        VarChar(100) NOT NULL,
-        NR_SENHA        VarChar(20) NOT NULL,
-        DS_CPF          VARCHAR(14) NOT NULL
+CREATE TABLE TB_Venda (
+        ID_Venda        INT PRIMARY KEY auto_increment,
+        ID_Cliente      int,
+        ID_Funcionario  int,
+        NM_Modelo       VarChar(100) NOT NULL,
+        NR_Placa        VarChar(100) NOT NULL,
+        DS_Email        VarChar(100) NOT NULL,
+        VL_Preco        DECIMAL(10, 2) NOT NULL,
+        DT_Compra       DATETIME NOT NULL,
+        FOREIGN KEY (ID_Cliente)        REFERENCES TB_Cliente(ID_Cliente),
+        FOREIGN KEY (ID_Funcionario)    REFERENCES TB_Funcionario(ID_Funcionario)
 );
