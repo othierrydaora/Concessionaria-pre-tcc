4bff49f522d6f47d3ee5e58cfc68201d54119e2d
Thiago Rodrigues <contatopixthiago@gmail.com>
2022-06-01 12:25:12 -0300

    arrumando

--- Scripts SQL/sistemaSigma.sql
+++ Scripts SQL/sistemaSigma.sql
@@ -14,7 +14,7 @@ CREATE TABLE TB_Cliente (
 CREATE TABLE TB_Venda (
         ID_Venda      INT PRIMARY KEY auto_increment,
         NM_MODELO     VarChar(100) NOT NULL,
-        NR_PLACA      VarChar(100) NOT NULL,
+        ds_PLACA      VarChar(8) NOT NULL,
         DS_EMAIL      VarChar(100) NOT NULL,
         VL_PRECO      DECIMAL(10, 2) NOT NULL,
         DT_COMPRA     DATETIME NOT NULL,
@@ -22,10 +22,9 @@ CREATE TABLE TB_Venda (
         CONSTRAINT FK_FuncionarioVenda  FOREIGN KEY (ID_Funcionario)    REFERENCES TB_Funcionario(ID_Funcionario)
 );
 
-CREATE TABLE TB_Funcionario (
-        ID_FUNCIONARIO  INT PRIMARY KEY,
-        NM_NOME         VarChar(100) NOT NULL,
-        DS_EMAIL        VarChar(100) NOT NULL,
-        NR_SENHA        VarChar(20) NOT NULL,
-        DS_CPF          VARCHAR(14) NOT NULL
+CREATE TABLE tb_funcionario (
+        id_funcionario  INT PRIMARY KEY,
+        nm_funcionario         VarChar(100) NOT NULL,
+        ds_email        VarChar(100) NOT NULL,
+        ds_senha        VarChar(20) NOT NULL
 );
