23a3bc1cc67dd9fdb4cf924608f6a93c2ed8d32a
Nicholas <doctorredac@gmail.com>
2022-06-17 01:44:17 -0300

    esqueleto nova page

--- site/src/pages/cards/index.js
+++ site/src/pages/cards/index.js
@@ -1,5 +1,42 @@
 import React, { useState, useEffect } from 'react';
+import storage from 'local-storage';
 import { toast } from 'react-toastify';
 import Header from '../../components/Header';
 import Menu from '../../components/Menu';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import './index.scss';
+
+
+export default function cards() {
+    return (
+        <div className='cards'>
+            <Header user logo />
+            <Menu selecionado='cards' /> 
+
+            <main className='cards-content'>
+                <div className='main-container'>
+                        <div className='adm-top-bar-crd'>
+                                <div className='adm-search-field-crd'>
+                                    <input type='text' onFocus={(e) => (e.target.type = "date")} onBlur={(e) => (e.target.type = "text")} placeholder='Data de Inicio'/>
+                                        <img src="/assets/Icons/search-icon.svg" style={{width: "17px", cursor: "pointer"}} alt="Pesquisar"/>
+                                </div>
+                                <div className='adm-search-field-crd'>
+                                    <input type='text' onFocus={(e) => (e.target.type = "date")} onBlur={(e) => (e.target.type = "text")} placeholder='Data final'/>
+                                    <img src="/assets/Icons/search-icon.svg" style={{width: "17px", cursor: "pointer"}} alt="Pesquisar"/>
+                                </div>
+                    </div>
+                    
+                    <div className='layout-card'>
+                        <img src='' alt='' style={{ height: '8em', width: '100%' }} />
+                        <ul className='card-text'>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+            </main>
+        </div>
+    )
+}
